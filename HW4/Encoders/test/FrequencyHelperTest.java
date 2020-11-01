247f1abcd2cfc2fae0366bfac43ce07c5afdabcd
Greg Attra <greg.attra@gmail.com>
2020-10-31 20:18:30 -0400

    added tests for freq entry and helper

---
+++ HW4/Encoders/test/FrequencyHelperTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import java.util.Stack;
+
+import codes.trees.CodeNode;
+import codes.trees.PrefixCodeLeaf;
+import codes.utils.Frequency;
+import codes.utils.FrequencyHelper;
+
+import static org.junit.Assert.assertEquals;
+
+public class FrequencyHelperTest {
+  @Test
+  public void testToStack() {
+    String sequence = "abcddeefg";
+    String codes = "01";
+    Stack<Frequency<CodeNode<String, String>>> stack =
+            FrequencyHelper.toStack(sequence.split(""), c -> new PrefixCodeLeaf(c));
+    assertEquals(7, stack.size());
+    for (Frequency<CodeNode<String, String>> entity : stack) {
+      if (entity.getValue().getSymbol().equals("d")
+          || entity.getValue().getSymbol().equals("e")) {
+        assertEquals(2, entity.getFrequency(), 0.0001);
+      } else {
+        assertEquals(1, entity.getFrequency(), 0.0001);
+      }
+    }
+  }
+}
