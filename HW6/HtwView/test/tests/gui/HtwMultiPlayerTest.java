174839aa3383948fd140e6a48a3754a4ca6e0824
Greg Attra <greg.attra@gmail.com>
2020-11-30 12:15:54 -0500

    workin on tests for gui

---
+++ HW6/HtwView/test/tests/gui/HtwMultiPlayerTest.java
@@ -0,0 +1,16 @@
+package tests.gui;
+
+import org.junit.Test;
+
+import gui.HtwMultiPlayer;
+import htw.game.IHtwPlayer;
+
+import static org.junit.Assert.assertEquals;
+
+public class HtwMultiPlayerTest {
+  @Test
+  public void testNumber() {
+    IHtwPlayer player = new HtwMultiPlayer("Joe", 1, 10);
+    assertEquals(1, player.number());
+  }
+}
