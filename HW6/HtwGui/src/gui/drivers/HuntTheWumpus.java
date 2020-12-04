11b2faef8f80cbc75ef34fded443f83416c0f0a3
Greg Attra <greg.attra@gmail.com>
2020-12-04 13:22:03 -0500

    working jar

--- HW6/HtwGui/src/gui/drivers/HuntTheWumpus.java
+++ HW6/HtwGui/src/gui/drivers/HuntTheWumpus.java
@@ -1,17 +1,37 @@
 package gui.drivers;
 
+import java.util.Scanner;
+
 import gui.GuiController;
 import gui.GuiView;
 import gui.IGuiController;
 import gui.IView;
+import htw.game.ConsoleController;
+import htw.game.HtwCommandMapFactory;
+import htw.game.commands.DirActionStrategy;
+import htw.game.commands.IActionStrategy;
+import htw.game.commands.IdActionStrategy;
 
 public class HuntTheWumpus {
   public static void main(String[] args) {
-//    String mode = args[0];
-//    System.out.printf("%s", mode);
-    IView view = new GuiView();
-    IGuiController controller = new GuiController(view);
-    view.setFeatures(controller);
-    controller.startNew();
+    String mode = args[0];
+    if (mode.equals("--gui")) {
+      IView view = new GuiView();
+      IGuiController controller = new GuiController(view);
+      view.setFeatures(controller);
+      controller.startNew();
+    } else if (mode.equals("--text")) {
+      IActionStrategy strategy = new DirActionStrategy();
+      if (args.length > 1 && args[1].equalsIgnoreCase("--id")) {
+        strategy = new IdActionStrategy();
+      }
+
+      Runnable controller = new ConsoleController(
+              new Scanner(System.in),
+              System.out,
+              strategy,
+              new HtwCommandMapFactory());
+      controller.run();
+    }
   }
 }
